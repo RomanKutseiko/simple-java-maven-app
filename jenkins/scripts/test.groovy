1d568be8aa18efd1bc8220881bc51e4a95bfbe5c
Raman Kutseika <Raman.Kutseika@itechart-group.com>
2019-08-15 17:55:37 +0300

    update test.groovy

--- jenkins/scripts/test.groovy
+++ jenkins/scripts/test.groovy
@@ -15,7 +15,7 @@ println CLIENT_NAME
 println ENVIRONMENT_TYPE
 println VERSION
 
-('pwd').execute()
+println ('pwd').execute()
 
 //set -x
 //NAME=('mvn help:evaluate -Dexpression=project.name').execute()
